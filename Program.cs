b716c0a69a7fa04ff94a4356cf5e9a66843aef94
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-31 09:08:06 -0700

    finish program.cs, app  settings and csproj

---
+++ Program.cs
@@ -0,0 +1,79 @@
+using MessageBoard.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MessageBoard.Models;
+
+
+namespace MessageBoard;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+    
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddDbContext<AnimalShelterContext>(
+                      dbContextOptions => dbContextOptions
+                        .UseMySql(
+                          builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                          ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                        )
+                      )
+                    );
+
+    builder.Services.AddMvc();
+    builder.Services.AddControllers();
+    builder.Services.AddRazorPages();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddAuthentication();
+ 
+    var app = builder.Build();
+    
+    if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
+                app.UseHttpsRedirection();
+            }
+    
+    app.Run();
+  }
+}
