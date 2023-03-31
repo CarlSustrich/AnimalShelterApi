574987beb216d90302e750ee8e956317463162bc
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-03-31 11:43:35 -0700

    update appsettings & program to utilize access token

--- Program.cs
+++ Program.cs
@@ -48,7 +48,19 @@ public class Program
                       )
                     );
 
-    // builder.Services.AddAuthentication();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidateLifetime = true,
+          ValidateIssuerSigningKey = true,
+          ValidIssuer = builder.Configuration["Jwt:Issuer"],
+          ValidAudience = builder.Configuration["Jwt:Audience"],
+          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+      });
  
     var app = builder.Build();
     
